883a3583e95bcc43b2143fedcd3d70f0afdb68a1
Пронин Егор <mrzillagold@gmail.com>
2021-05-09 23:43:31 +1000

    fix(esm): import enums

--- esm/RawJSONBuilder.mjs
+++ esm/RawJSONBuilder.mjs
@@ -2,4 +2,4 @@ import _RawJSONBuilder from "../lib/RawJSONBuilder.js";
 
 export default _RawJSONBuilder;
 
-export const { RawJSONBuilder } = _RawJSONBuilder;
+export const { RawJSONBuilder, ClickAction, HoverAction, Color } = _RawJSONBuilder;
